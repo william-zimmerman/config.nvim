eb44d866e9eb447930e13074a3efacd53e94d0a4
William Zimmerman <will.zimmerman@gmail.com>
2025-01-11 16:51:14 +0000

    Adding haskell-tools.nvim

---
+++ lua/custom/plugins/haskell-tools.lua
@@ -0,0 +1,8 @@
+-- haskell-tools.nvim
+-- https://github.com/mrcjkb/haskell-tools.nvim
+
+return {
+  'mrcjkb/haskell-tools.nvim',
+  version = '^4', -- Recommended
+  lazy = false, -- This plugin is already lazy
+}
