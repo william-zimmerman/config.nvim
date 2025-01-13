a0c309c93a634baa51cd67de7a9d6b744cbc1c1b
William Zimmerman <will.zimmerman@gmail.com>
2025-01-13 16:40:48 +0000

    Code lens keybinding

--- init.lua
+++ init.lua
@@ -550,6 +550,9 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          -- Execute code lens
+          map('<leader>cl', vim.lsp.codelens.run, '[C]ode [L]ens')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
