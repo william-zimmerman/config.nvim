f1100d4c3ed5fd5fbb4a09eeb63debcca33bf338
William Zimmerman <will.zimmerman@gmail.com>
2025-01-13 16:40:21 +0000

    Evaluate All keybinding

--- lua/custom/plugins/haskell-tools.lua
+++ lua/custom/plugins/haskell-tools.lua
@@ -1,8 +1,15 @@
 -- haskell-tools.nvim
 -- https://github.com/mrcjkb/haskell-tools.nvim
 
-return {
+opts = {
   'mrcjkb/haskell-tools.nvim',
   version = '^4', -- Recommended
   lazy = false, -- This plugin is already lazy
+
+  config = function()
+    local ht = require 'haskell-tools'
+    vim.keymap.set('n', '<leader>ea', ht.lsp.buf_eval_all, { desc = '[E]valuate [A]ll Haskell expressions' })
+  end,
 }
+
+return opts
