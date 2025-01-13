72312338efd62e9723b202657185b4d77dab4c9f
William Zimmerman <will.zimmerman@gmail.com>
2025-01-13 17:03:00 +0000

    Linting haskell-tools

--- lua/custom/plugins/haskell-tools.lua
+++ lua/custom/plugins/haskell-tools.lua
@@ -1,7 +1,7 @@
 -- haskell-tools.nvim
 -- https://github.com/mrcjkb/haskell-tools.nvim
 
-opts = {
+local opts = {
   'mrcjkb/haskell-tools.nvim',
   version = '^4', -- Recommended
   lazy = false, -- This plugin is already lazy
