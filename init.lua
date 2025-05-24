98c7d726cd55b6ac78f6c2599af9f461d827d1a2
William Zimmerman <will.zimmerman@gmail.com>
2025-05-24 14:46:56 +0100

    Enabling diagnostic symbols in gutter

--- init.lua
+++ init.lua
@@ -593,14 +593,14 @@ require('lazy').setup({
       })
 
       -- Change diagnostic symbols in the sign column (gutter)
-      -- if vim.g.have_nerd_font then
-      --   local signs = { ERROR = '', WARN = '', INFO = '', HINT = '' }
-      --   local diagnostic_signs = {}
-      --   for type, icon in pairs(signs) do
-      --     diagnostic_signs[vim.diagnostic.severity[type]] = icon
-      --   end
-      --   vim.diagnostic.config { signs = { text = diagnostic_signs } }
-      -- end
+      if vim.g.have_nerd_font then
+        local signs = { ERROR = '', WARN = '', INFO = '', HINT = '' }
+        local diagnostic_signs = {}
+        for type, icon in pairs(signs) do
+          diagnostic_signs[vim.diagnostic.severity[type]] = icon
+        end
+        vim.diagnostic.config { signs = { text = diagnostic_signs } }
+      end
 
       -- Disable diagnostics (in liu of a diagnostics plugin)
       vim.diagnostic.config { virtual_text = false }
